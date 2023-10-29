e18cd475acf66ad480f3332263299cbc87f0b43b
dekings44@gmail.com <dekings44@gmail.com>
2023-10-29 17:52:46 +0000

    listed all files in container and imported all libraries

--- full_data_engineering_project.py
+++ full_data_engineering_project.py
@@ -32,3 +32,28 @@ dbutils.fs.mount(
 
 # Check the list of folders or files in the denormalized container
 dbutils.fs.ls("/mnt/denormalized")
+
+# COMMAND ----------
+
+data = '/mnt/supply-chain-raw/data_e06f395b-5a79-4bb5-b089-ae66af73fdf6_a3f3d274-99c8-44de-937c-5d6150a2811f.parquet'
+
+# COMMAND ----------
+
+supply_data = spark.read.format('parquet').load(data)
+
+# COMMAND ----------
+
+display(supply_data)
+
+# COMMAND ----------
+
+supply_data.schema.names
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import from_utc_timestamp, date_format
+from pyspark.sql.functions import year, quarter, month, weekofyear, dayofmonth, hour
+from pyspark.sql.types import TimestampType
+spark = SparkSession.builder.appName("datetime_columns_extraction").getOrCreate()
