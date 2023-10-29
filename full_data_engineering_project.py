674cbfb1d3e7d54e8db76220194cdb3a3204f17a
dekings44@gmail.com <dekings44@gmail.com>
2023-10-29 17:47:23 +0000

    mounted the transformed container

--- full_data_engineering_project.py
+++ full_data_engineering_project.py
@@ -14,3 +14,21 @@ dbutils.fs.mount(
 
 # Check the list of folders or files in the container
 dbutils.fs.ls("/mnt/supply-chain-raw")
+
+# COMMAND ----------
+
+configs = {
+  "fs.azure.account.auth.type": "CustomAccessToken",
+  "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
+}
+
+# Mounting the denormalized data container.
+dbutils.fs.mount(
+  source = "abfss://denormalized@supplychainobject.dfs.core.windows.net/",
+  mount_point = "/mnt/denormalized",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+# Check the list of folders or files in the denormalized container
+dbutils.fs.ls("/mnt/denormalized")
